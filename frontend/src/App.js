caaec7a0b26c3a465a9479a93f1f222b9102743c
Theo <sagniez.theo@laposte.net>
2022-05-17 17:42:04 +0200

    Added search functionality + possibility to enter quantity

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,10 +6,24 @@ import FoodInfo from './components/FoodInfo.js'
 
 function App() {
   const [data, setData] = useState()
-  const [foodInfo, setFoodInfo] = useState({})
+  const [foodInfo, setFoodInfo] = useState()
+  const [foodSearch, setFoodSearch] = useState('')
+  const [quantity, setQuantity] = useState('')
 
   const displayInfo = (id) => {
     setFoodInfo(data.find(item => item.id === id))
+  }
+
+  const getFood = (food) => {
+    fetch(`http://localhost:3001/${food}`)
+      .then(res => res.json())
+      //data.data[0] because the response is an array with one object in it
+      .then(data => {
+        setFoodInfo(data.data[0])
+        setFoodSearch('')
+        setQuantity('')
+      })
+      .catch(err => console.log(err))
   }
 
   useEffect(() => {
@@ -20,38 +34,64 @@ function App() {
   }, [])
 
   return (
-    <div>
-      <div className='d-flex flex-column app_div_buttons'>
-        { !data ? 
-              'Loading...' 
-              : 
-              data.map(item => (
-                  <Button key={item.id} 
-                          className='m-1'
-                          onClick={() => displayInfo(item.id)}
-                  >
-                    {item.name[0].toUpperCase() + item.name.slice(1)}
-                  </Button>
-                )
-              ) 
-        }
+    <>
+      <div className='d-flex flex-column text-center w-25 m-auto'>
+        <p className='mb-0'>Search a food</p>
+        <input type='text' 
+               className='mb-1 p-1'
+               value={foodSearch}
+               onChange={(e) => setFoodSearch(e.target.value)}        
+        />
+        <p className='mb-0'>Quantity</p>
+        <input type='text' 
+               className='mb-1 p-1'
+               value={quantity}
+               onChange={(e) => setQuantity(e.target.value)}        
+        />
+        <Button variant='dark' 
+                className='app_searchButton m-auto mb-2'
+                onClick={(food) => getFood(foodSearch)}
+        >Search
+        </Button>
       </div>
-      {
-        foodInfo ?
-          <FoodInfo
-            name={foodInfo.name}
-            calories={foodInfo.calories}
-            protein={foodInfo.protein}
-            carbs={foodInfo.carbs}
-            sugar={foodInfo.sugar}
-            fat={foodInfo.fat}
-            saturatedFat={foodInfo.saturatedFat}
-            fiber={foodInfo.fiber} 
-          />
-          :
-          null
-      }
-    </div> 
+      
+      <div className='d-flex'>
+        <div className='d-flex flex-column app_div_buttons'>
+          { !data ? 
+                'Loading...' 
+                : 
+                data.map(item => (
+                    <Button key={item.id} 
+                            className='m-1'
+                            onClick={() => displayInfo(item.id)}
+                    >
+                      {item.name[0].toUpperCase() + item.name.slice(1)}
+                    </Button>
+                  )
+                ) 
+          }
+        </div>
+        <div className='flex-fill app_div_foodInfo'>
+          {
+            foodInfo ?
+              <FoodInfo
+                name={foodInfo.name}
+                calories={foodInfo.calories}
+                protein={foodInfo.protein}
+                carbs={foodInfo.carbs}
+                sugar={foodInfo.sugar}
+                fat={foodInfo.fat}
+                saturatedFat={foodInfo.saturatedFat}
+                fiber={foodInfo.fiber} 
+                quantity={quantity === '' ? 100 : parseInt(quantity)}
+              />
+              :
+              null
+          }
+        </div>
+      </div> 
+    </>
+    
   );
 }
 
--- frontend/src/components/FoodInfo.js
+++ frontend/src/components/FoodInfo.js
@@ -2,46 +2,49 @@ import React from 'react'
 import Table from 'react-bootstrap/Table'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-const FoodInfo = ({ name, calories, protein, carbs, sugar, fat, saturatedFat, fiber }) => {
+const FoodInfo = ({ name, calories, protein, carbs, sugar, fat, saturatedFat, fiber, quantity }) => {
+  const calculate = (value, quantity) => {
+    return Math.round(((quantity / 100) * value) * 10) / 10
+  }
+  
   return (
     <div>
-
       <Table striped className='caption-top'>
-        <caption>{name}</caption>
+        <caption className='text-center'>{name[0].toUpperCase() + name.slice(1)}</caption>
         <thead>
           <tr>
             <th>Nutritional value for</th>
-            <th>100 grams</th>
+            <th>{quantity} grams</th>
           </tr>
         </thead>
         <tbody>
           <tr>
             <td>Calories</td>
-            <td>{calories} kcal</td>
+            <td>{calories === null ? 'Insignificant' : calculate(calories, quantity) + ' kcal'}</td>
           </tr>
           <tr>
             <td>Protein</td>
-            <td>{protein} grams</td>
+            <td>{protein === null ? 'Insignificant' : calculate(protein, quantity) + ' grams'}</td>
           </tr>
           <tr>
             <td>Carbs</td>
-            <td>{carbs} grams</td>
+            <td>{carbs === null ? 'Insignificant' : calculate(carbs, quantity) + ' grams'}</td>
           </tr>
           <tr>
             <td>Sugar</td>
-            <td>{sugar} grams</td>
+            <td>{sugar === null ? 'Insignificant' : calculate(sugar, quantity) + ' grams'}</td>
           </tr>
           <tr>
             <td>Fat</td>
-            <td>{fat} grams</td>
+            <td>{fat === null ? 'Insignificant' : calculate(fat, quantity) + ' grams'}</td>
           </tr>
           <tr>
             <td>Saturated fat</td>
-            <td>{saturatedFat} grams</td>
+            <td>{saturatedFat === null ? 'Insignificant' : calculate(saturatedFat, quantity) + ' grams'}</td>
           </tr>
           <tr>
             <td>Fiber</td>
-            <td>{fiber} grams</td>
+            <td>{fiber === null ? 'Insignificant' : calculate(fiber, quantity) + ' grams'}</td>
           </tr>
         </tbody>
       </Table>
