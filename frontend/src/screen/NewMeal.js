de821367a1eccb0f3cbaac9fb512391ab3060722
Theo <sagniez.theo@laposte.net>
2022-05-24 17:38:15 +0200

    Started working on composing a meal functionality

--- frontend/src/screen/NewMeal.js
+++ frontend/src/screen/NewMeal.js
@@ -8,6 +8,7 @@ const NewMeal = () => {
     const [foodSearch, setFoodSearch] = useState('')
     const [matches, setMatches] = useState()
     const [invalidSearch, setInvalidSearch] = useState('')
+    const [meal, setMeal] = useState([])
   
   
     const getFood = (food) => {
@@ -29,10 +30,15 @@ const NewMeal = () => {
         })
         .catch(err => console.log(err))
     }
+
+    const addToMeal = (food) => {
+      setMeal([...meal, food])
+    }
   
     return (
       <>    
-        <Navbar />  
+        <Navbar /> 
+
         <div className='d-flex flex-column text-center w-25 m-auto'>
           <p className='mb-0'>Search a food</p>
           <input type='text' 
@@ -48,6 +54,27 @@ const NewMeal = () => {
         </div>
         
         <div className='d-flex'>
+          <div className='p-1'>
+            {
+              meal.length > 0 ?
+                  meal.map(item => {
+                    return (
+                      <div className='d-flex mb-1'>
+                        <p>{item[0].toUpperCase() + item.slice(1)}</p>
+                        <input type='text' 
+                               placeholder='Quantity' 
+                               className='ms-1 w-25 h-25 p-1' 
+                        />
+                      </div>
+                    ) 
+                  })
+                :
+                null
+            }
+            <Button className='mt-1' disabled>
+              Save meal
+            </Button>
+          </div>
           <div className='d-flex flex-fill justify-content-center align-items-center'>
             {
                 matches ?
@@ -57,16 +84,14 @@ const NewMeal = () => {
                       matches.map(match => {
                         return  <Button 
                                   key={match.id}
-                                  onClick={() => getFood(match.name)}
+                                  onClick={() => addToMeal(match.name)}
                                   className='mb-1'
                                   variant="link"
                                 >
                                   {match.name[0].toUpperCase() + match.name.slice(1)}
                                 </Button>
-                          
                       })
                     }
-                  
                   </div>
                   :
                   //To avoid getting an error
