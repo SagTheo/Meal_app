c02134279a79d3f539753260db534e107781ce6b
Theo <sagniez.theo@laposte.net>
2022-06-06 17:40:51 +0200

    Todo.txt: worked on Issue#1

--- frontend/src/screen/NewMeal.js
+++ frontend/src/screen/NewMeal.js
@@ -9,6 +9,15 @@ const NewMeal = () => {
     const [matches, setMatches] = useState()
     const [invalidSearch, setInvalidSearch] = useState('')
     const [meal, setMeal] = useState([])
+    const [totalNutriValues, setTotalNutriValues] = useState({
+      calories: 0,
+      protein: 0,
+      carbs: 0,
+      sugar: 0,
+      fat: 0,
+      saturatedFat: 0,
+      fiber: 0
+    })
   
   
     const getFood = (food) => {
@@ -32,11 +41,22 @@ const NewMeal = () => {
     }
 
     const addToMeal = (food) => {
-      setMeal([...meal, food])
+      setMeal([...meal, {...food, quantity: 100}])
     }
 
     const removeFromMeal = (food) => {
       setMeal(meal.filter(meal => meal.id !== food.id))
+    }
+
+    //Calculates the total amount of 'value' for the meal
+    const total = (array) => {
+      const values = ['calories', 'protein', 'carbs', 'sugar', 'fat', 'saturateFat', 'fiber']
+
+      values.forEach(value => {
+        setTotalNutriValues({
+          ...totalNutriValues, 
+          totalNutriValues[value]: array.reduce((acc, curr) => acc + (curr.quantity / 100 * JSON.parse(curr[value])), 0)})
+      })
     }
   
     return (
@@ -68,12 +88,13 @@ const NewMeal = () => {
                         <input type='text' 
                                placeholder='Quantity' 
                                className='ms-1 w-50 h-25 p-1' 
+                               onChange={(e) => item.quantity = e.target.value}
                         />
                         <svg xmlns="http://www.w3.org/2000/svg" 
                               width="16" 
                               height="16" 
-                              fill="currentColor" 
-                              className="bi bi-trash" 
+                              fill="black" 
+                              className="bi bi-trash trashcan" 
                               viewBox="0 0 16 16"
                               onClick={() => removeFromMeal(item)}
                         >
@@ -89,6 +110,9 @@ const NewMeal = () => {
             <Button className='mt-1' disabled>
               Save meal
             </Button>
+            <Button className='mt-1' onClick={() => total(meal)}>
+              Update meal
+            </Button>
           </div>
             {
                 matches ?
@@ -98,7 +122,7 @@ const NewMeal = () => {
                       matches.map(match => {
                         return  <Button 
                                   key={match.id}
-                                  onClick={() => addToMeal({'id': match.id, 'name': match.name})}
+                                  onClick={() => addToMeal(match)}
                                   className='mb-1'
                                   variant="link"
                                 >
@@ -113,6 +137,15 @@ const NewMeal = () => {
             }
           <div className='p-1 w-25'>
             <p>Nutritional values for your meal</p>
+            <ul>
+              <li>Calories: {totalNutriValues.calories} grams</li>
+              <li>Protein: {totalNutriValues.protein} grams</li>
+              <li>Carbs: {totalNutriValues.carbs} grams</li>
+              <li>Sugar: {totalNutriValues.sugar} grams</li>
+              <li>Fat: {totalNutriValues.fat} grams</li>
+              <li>Saturated fat: {totalNutriValues.saturatedFat} grams</li>
+              <li>Fiber: {totalNutriValues.fiber} grams</li>
+            </ul>
           </div>
         </div> 
       </>
